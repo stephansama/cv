1db5998e1fa5c05b1a23d78dbebf7fb3f89f3406
stephansama <stephanrandle@hotmail.com>
2026-02-10 09:16:19 -0500

    chore: dogfooded nocodb api

--- scripts/spy-sync.ts
+++ scripts/spy-sync.ts
@@ -4,94 +4,89 @@ import * as url from "node:url";
 import * as z from "zod";
 import * as schema from "./spy-schema";
 
+import { createApi } from "@stephansama/typed-nocodb-api";
+
 const envSchema = z.object({
-	NOCODB_JOB_URL: z.string().min(1),
+	NOCODB_COMPANY_TABLE: z.string().min(1),
+	NOCODB_CAREERS_BASE: z.string().min(1),
+	NOCODB_JOB_TABLE: z.string().min(1),
 	NOCODB_TOKEN: z.string().min(1),
+	NOCODB_URL: z.string().min(1),
 });
 
 const env = envSchema.parse(process.env);
+
+const careersApi = createApi({
+	token: env.NOCODB_TOKEN,
+	baseId: env.NOCODB_CAREERS_BASE,
+	origin: env.NOCODB_URL,
+	tableId: env.NOCODB_COMPANY_TABLE,
+	schema: z.object({
+		"name": z.string(),
+		"description": z.string(),
+		"url": z.string(),
+		"careers page": z.string(),
+		"jobs": z.object({
+			id: z.number(),
+		}),
+	}),
+});
+
+const jobApi = createApi({
+	token: env.NOCODB_TOKEN,
+	baseId: env.NOCODB_CAREERS_BASE,
+	origin: env.NOCODB_URL,
+	tableId: env.NOCODB_JOB_TABLE,
+	schema: z.object({
+		"position title": z.string(),
+		"url": z.string(),
+		"min salary": z.number(),
+		"max salary": z.number(),
+		"company": z.object({ id: z.number() }),
+		"follow up": z.string().optional().nullable(),
+		"type": z.enum(["fulltime", "parttime", "internship", "contract"]),
+		"pay_interval": z.enum(["yearly", "monthly", "daily", "hourly"]),
+		"stage": z.enum([
+			"interested",
+			"applied",
+			"interview",
+			"need to follow up",
+		]),
+		"source": z.enum([
+			"website",
+			"indeed",
+			"linkedin",
+			"zip_recruiter",
+			"google",
+			"glassdoor",
+		]),
+	}),
+});
+
 const basepath = path.dirname(url.fileURLToPath(import.meta.url));
 const file = await fsp.readFile(path.join(basepath, "../jobs.json"), "utf8");
-const found = schema.jobSpyListSchema.parse(JSON.parse(file));
+const foundPositions = schema.jobSpyListSchema.parse(JSON.parse(file));
 
-const ACTIONS = ["LIST", "CREATE", "UPDATE", "DELETE"] as const;
-type ACTION = (typeof ACTIONS)[number];
-
-const API = {
-	LIST: {
-		method: "get",
-		responseSchema: z.object({
-			records: z.array(
-				z.object({
-					id: z.string(),
-					fields: z.object({
-						"position title": z.string(),
-						"url": z.string(),
-						"stage": z.string(),
-						"source": z.string(),
-						"min salary": z.number(),
-						"max salary": z.number(),
-						"follow up": z.string(),
-					}),
-				}),
-			),
-			next: z.string(),
-			prev: z.string(),
-			nestedNext: z.string(),
-			nestedPrev: z.string(),
-		}),
-		url: env.NOCODB_JOB_URL,
-	},
-	CREATE: {
-		method: "post",
-		inputSchema: z.object(),
-		responseSchema: z.object(),
-		url: env.NOCODB_JOB_URL,
-	},
-	UPDATE: {
-		method: "patch",
-		inputSchema: z.object(),
-		responseSchema: z.object(),
-		url: env.NOCODB_JOB_URL,
-	},
-	DELETE: {
-		method: "patch",
-		inputSchema: z.object(),
-		responseSchema: z.object(),
-		url: env.NOCODB_JOB_URL,
-	},
-} satisfies Partial<
-	Record<
-		ACTION,
+const companies = Object.fromEntries(
+	foundPositions.map((position) => [
+		position.company,
 		{
-			method: "delete" | "get" | "patch" | "post" | "put";
-			inputSchema?: z.ZodType;
-			responseSchema: z.ZodType;
-			url: string;
-		}
-	>
->;
+			url: position.company_url,
+			description: position.company_description,
+		},
+	]),
+);
 
-async function loadApi<A extends ACTION, T extends (typeof API)[A]>(
-	action: A,
-	input?: "inputSchema" extends keyof T ? z.infer<T["inputSchema"]> : never,
-) {
-	const current = API[action];
-	const body =
-		"inputSchema" in current
-			? JSON.stringify(current.inputSchema.parse(input))
-			: undefined;
-	const headers = new Headers({
-		"accept": "application/json",
-		"xc-token": env.NOCODB_TOKEN,
-	});
+const nocodbList = await jobApi.fetch({ action: "LIST" });
 
-	const response = await fetch(current.url, {
-		method: current.method,
-		body,
-		headers,
-	});
+await fsp.writeFile(
+	"./output.json",
+	JSON.stringify(nocodbList, undefined, 2),
+	"utf8",
+);
 
-	const json = await response.json();
-	return current.responseSchema.parse(json);
-}
+// Find companies from scraped jobs
+// List saved companies
+// save companies not in the list
+// List saved jobs
+// save jobs not in the list with company id
